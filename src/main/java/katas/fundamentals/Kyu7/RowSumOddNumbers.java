2971d3fe824c17da38dc57578d2bb468caf8bfa8
Morgiahn <19307448+Morgiahn@users.noreply.github.com>
2025-02-24 12:09:02 +0100

    RowSumOddNumbers refacto2

--- src/main/java/katas/fundamentals/Kyu7/RowSumOddNumbers.java
+++ src/main/java/katas/fundamentals/Kyu7/RowSumOddNumbers.java
@@ -1,6 +1,8 @@
 package katas.fundamentals.Kyu7;
 
 import java.util.ArrayList;
+import java.util.stream.IntStream;
+
 /*
 Ginven the triangle of consecutive odd numbers:
 
@@ -15,22 +17,20 @@ Ginven the triangle of consecutive odd numbers:
  */
 public class RowSumOddNumbers {
     public static int rowSumOddNumbers(int n) {
-
-        return somme(n);
+        int start = n * (n  - 1) + 1;
+        return  totalNumbers(start, n);
     }
 
-    public static int somme(int rows) {
-        int number = 1;
-        int row = 0;
-        int total = 0;
-        while(row < rows) {
-            row ++;
-            total = 0;
-            for (int numberInRows = 0; numberInRows < row; numberInRows++) {
-                total += number;
-                number += 2;
-            }
+    public static int totalNumbers(int number, int row) {
+        int  total = 0;
+        for (int numberInRows = 0; numberInRows < row; numberInRows++) {
+            total += number;
+            number += 2;
         }
         return total;
+    }
+
+    public static int getFirstNumber(int rows) {
+        return rows * (rows  - 1) + 1;
     }
 }
--- src/test/java/katas/fundamentals/Kyu7/RowSumOddNumbersTest.java
+++ src/test/java/katas/fundamentals/Kyu7/RowSumOddNumbersTest.java
@@ -9,12 +9,36 @@ class RowSumOddNumbersTest {
     @Test
     public void test1() {
         assertEquals(1, RowSumOddNumbers.rowSumOddNumbers(1));
-
-        assertEquals(74088, RowSumOddNumbers.rowSumOddNumbers(42));
     }
 
     @Test
     void test_3_5_retrun_8() {
         assertEquals(8, RowSumOddNumbers.rowSumOddNumbers(2));
+    }
+
+    @Test
+    public void test42() {
+
+        assertEquals(74088, RowSumOddNumbers.rowSumOddNumbers(42));
+    }
+
+
+    @Test
+    void getFirstNumber_row1() {
+        assertEquals(1, RowSumOddNumbers.getFirstNumber(1));
+    }
+
+    @Test
+    void getFirstNumber_row2() {
+        assertEquals(3, RowSumOddNumbers.getFirstNumber(2));
+    }
+    @Test
+    void getFirstNumber_row3() {
+        assertEquals(7, RowSumOddNumbers.getFirstNumber(3));
+    }
+
+    @Test
+    void getFirstNumber_row4() {
+        assertEquals(13, RowSumOddNumbers.getFirstNumber(4));
     }
 }
