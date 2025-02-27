2efa80b36e4ce363ccf2683f2c519cf1ee7eaccc
Morgiahn <19307448+Morgiahn@users.noreply.github.com>
2025-02-27 12:26:44 +0100

    HighestScoringWord

---
+++ src/main/java/katas/fundamentals/kyu6/HighestScoringWord.java
@@ -0,0 +1,39 @@
+package katas.fundamentals.kyu6;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/*
+
+Given a string of words, you need to find the highest scoring word.
+
+Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+For example, the score of abad is 8 (1 + 2 + 1 + 4).
+
+You need to return the highest scoring word as a string.
+
+If two words score the same, return the word that appears earliest in the original string.
+
+All letters will be lowercase and all inputs will be valid.
+
+
+ */
+public class HighestScoringWord {
+    public static String high(String s) {
+        // Your code here...
+        var r  = Arrays.stream(s.split("\\s+"))
+                .sorted(Comparator.comparing(w -> -wordValue(w)))
+                .findFirst();
+        return r.get();
+    }
+
+    static int wordValue(String s) {
+        return s.chars().mapToObj(i -> i - 96).reduce( 0, (x, y ) -> x = x+ y);
+    }
+}
+
+//                .orElse("0");
+//                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
---
+++ src/test/java/katas/fundamentals/kyu6/HighestScoringWordTest.java
@@ -0,0 +1,32 @@
+package katas.fundamentals.kyu6;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HighestScoringWordTest {
+
+    @Test
+    public void sampleTests() {
+        assertEquals("taxi", HighestScoringWord.high("man i need a taxi up to ubud"));
+        assertEquals("volcano", HighestScoringWord.high("what time are we climbing up to the volcano"));
+        assertEquals("semynak", HighestScoringWord.high("take me to semynak"));
+    }
+
+    @Test
+    public void edgeCaseTests() {
+        assertEquals("aa", HighestScoringWord.high("aa b"));
+        assertEquals("b", HighestScoringWord.high("b aa"));
+        assertEquals("bb", HighestScoringWord.high("bb d"));
+        assertEquals("d", HighestScoringWord.high("d bb"));
+        assertEquals("aaa", HighestScoringWord.high("aaa b"));
+    }
+
+    @Test
+    public  void testWordValue() {
+        assertEquals(1, HighestScoringWord.wordValue("a"));
+        assertEquals(2, HighestScoringWord.wordValue("b"));
+        assertEquals(27, HighestScoringWord.wordValue("az"));
+        assertEquals(8, HighestScoringWord.wordValue("abad"));
+    }
+}
