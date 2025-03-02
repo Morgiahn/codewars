972bfbec7f62c9f5196ae60a88cf8b51ed2e75af
PONCHON Marc <marc.ponchon@orange.com>
2025-03-02 12:06:57 +0100

    SmallestIntegerFinder

---
+++ src/main/java/katas/fundamentals/kyu8/SmallestIntegerFinder.java
@@ -0,0 +1,34 @@
+package katas.fundamentals.kyu8;
+
+import java.util.Arrays;
+
+public class SmallestIntegerFinder {
+
+    public static int findSmallestInt(int[] args) {
+        return Arrays.stream(args).min().orElse(0);
+    }
+
+
+}
+
+/*
+
+    public static int findSmallestInt(int[] args) {
+        return IntStream.of(args).min().getAsInt();
+    }
+
+    public static int findSmallestInt(int[] args) {
+      return Arrays.stream(args).sorted().toArray()[0];
+    }
+
+   public static int findSmallestInt(int[] args) {
+        Arrays.sort(args);
+        return args[0];
+    }
+
+
+    public static int findSmallestInt(int[] args) {
+      return Arrays.stream(args).summaryStatistics().getMin();
+    }
+
+ */
---
+++ src/test/java/katas/fundamentals/kyu8/SmallestIntegerFinderTest.java
@@ -0,0 +1,24 @@
+package katas.fundamentals.kyu8;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SmallestIntegerFinderTest {
+    @Test
+    void sampleTests() {
+        doTest(new int[]{78,56,-2,12,-8}, -8);
+        doTest(new int[]{Integer.MIN_VALUE, -666}, Integer.MIN_VALUE);
+        doTest(new int[]{666, Integer.MIN_VALUE}, Integer.MIN_VALUE);
+        doTest(new int[]{Integer.MAX_VALUE, Integer.MAX_VALUE}, Integer.MAX_VALUE);
+        doTest(new int[]{Integer.MIN_VALUE, Integer.MIN_VALUE}, Integer.MIN_VALUE);
+    }
+
+    private static void doTest(int[] input, int expected) {
+        String message = String.format("for input %s\n", Arrays.toString(input));
+        int actual = SmallestIntegerFinder.findSmallestInt(input);
+        assertEquals(expected, actual, message);
+    }
+}
