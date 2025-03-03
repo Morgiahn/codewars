b9aa3d590b2e1b620fcd95ab5d49a03686abdd05
PONCHON Marc <marc.ponchon@orange.com>
2025-03-03 08:06:10 +0100

    TwoToOne string

---
+++ src/main/java/katas/fundamentals/Kyu7/TwoToOne.java
@@ -0,0 +1,45 @@
+package katas.fundamentals.Kyu7;
+
+
+/*
+Take 2 strings s1 and s2 including only letters from a to z.
+Return a new sorted string (alphabetical ascending), the longest possible,
+containing distinct letters - each taken only once - coming from s1 or s2.
+Examples:
+
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+
+
+ */
+public class TwoToOne {
+    public static String longest (String s1, String s2) {
+        // your code
+        return (s1 + s2).chars()
+                .distinct()
+                .sorted()
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+//                .collect(StringBuilder::new, (sb, i) -> sb.append((char) i), StringBuilder::append).toString();
+    }
+}
+
+/*
+     public static String longest (String s1, String s2) {
+        StringBuilder sb = new StringBuilder();
+        (s1 + s2).chars().distinct().sorted().forEach(c -> sb.append((char) c));
+        return sb.toString();
+    }
+
+    public static String longest (String s1, String s2) {
+        return Stream.of(s1.concat(s2).split(""))
+                  .sorted()
+                  .distinct()
+                  .collect(Collectors.joining());
+    }
+
+ */
---
+++ src/test/java/katas/fundamentals/Kyu7/TwoToOneTest.java
@@ -0,0 +1,17 @@
+package katas.fundamentals.Kyu7;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TwoToOneTest {
+
+    @Test
+    public void test() {
+        System.out.println("longest Fixed Tests");
+        assertEquals("aehrsty", TwoToOne.longest("aretheyhere", "yestheyarehere"));
+        assertEquals("abcdefghilnoprstu", TwoToOne.longest("loopingisfunbutdangerous", "lessdangerousthancoding"));
+        assertEquals("acefghilmnoprstuy", TwoToOne.longest("inmanylanguages", "theresapairoffunctions"));
+    }
+}
