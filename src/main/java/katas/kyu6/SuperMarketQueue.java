29506a376c920abd24237c1e5469f89cbcfc4c72
Morgiahn <19307448+Morgiahn@users.noreply.github.com>
2025-03-22 16:06:12 +0100

    wip SuperMarketQueue1

--- src/main/java/katas/kyu6/SuperMarketQueue.java
+++ src/main/java/katas/kyu6/SuperMarketQueue.java
@@ -1,6 +1,10 @@
 package katas.kyu6;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 /*
 There is a queue for the self-checkout tills at the supermarket.
@@ -21,18 +25,94 @@ output
 The function should return an integer, the total time required.
 
  */
+
 public class SuperMarketQueue {
+
+    public static Logger log = Logger.getLogger("SuperMarketQueue");
+
+    static {
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setFormatter(new SimpleFormatter() {
+            @Override
+            public String format(LogRecord record) {
+                return String.format("[%s] %s - %s\n",
+                        record.getLevel(),
+                        record.getLoggerName(),
+                        record.getMessage());
+            }
+        });
+        log.setUseParentHandlers(false);
+        log.addHandler(handler);
+    }
+
+    static class Till {
+        int time;
+        Till(int time) {
+            this.time = time;
+        }
+        void setTime(int t) { time = t; }
+        boolean isAvailable() { return time == 0; }
+        void update() {
+            time--;
+            if (time < 0) { time = 0; }
+        }
+    }
 
     public static int solveSuperMarketQueue(int[] customers, int n) {
 
         if (customers.length == 0) {  return 0; }
-        if (customers.length < n ) {
-            n =  customers.length;
+        LinkedList<Integer> stackFifo =  new LinkedList<>();
+        for(int c : customers) {
+            stackFifo.add(c);
         }
-        int mini =  Arrays.stream(customers).sum()/n;
 
-        if (mini < n) { mini = Math.min(n, customers.length) ; }
-        return mini;
+        List<Till> tills = new ArrayList<>();
+        for (int i =0; i<n;i++) {
+            tills.add(new Till(0));
+        }
+
+        int totalTime = 0;
+        while(!stackFifo.isEmpty() || anyTillIsBusy(tills) ) {
+            totalTime++;
+            // execution en parallele
+            String stills = "";
+            for(int i=0; i<n; i++) {
+                Till till = tills.get(i);
+                // check till
+                String operation = " -";
+                if (till.isAvailable() && !stackFifo.isEmpty()) {
+                    int customerTime = stackFifo.remove();
+                    till.setTime(customerTime);
+                    operation = "<" + customerTime;
+                }
+                till.update();
+                stills += " till("+i + "):" + operation;
+            }
+
+            log.info("round " + totalTime + " " +  stills + " queue : " + stackFifo.size());
+            log.info(debugTime( tills, stackFifo, totalTime, n));
+        }
+        // all customers gone and all tills are free
+        return totalTime;
     }
+
+    static boolean anyTillIsBusy(List<Till> tills) {
+        for (Till till: tills) {
+            if (!till.isAvailable()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static String debugTime(List<Till> tills, LinkedList<Integer> stackFifo, int totalTime, int n) {
+
+        String stills = "";
+        for(int i = 0; i < n; i++ ) {
+            stills += " till("+i + "): " + tills.get(i).time;
+        }
+        return "round " + totalTime + " " + stills + " queue : " + stackFifo.size();
+    }
+
 
 }
--- src/test/java/katas/kyu6/SuperMarketQueueTest.java
+++ src/test/java/katas/kyu6/SuperMarketQueueTest.java
@@ -25,4 +25,60 @@ class SuperMarketQueueTest {
     void custumer2_and5tills() {
         assertEquals(2, SuperMarketQueue.solveSuperMarketQueue(new int[]{2}, 5));
     }
+
+    // For customers [6, 1, 1, 2, 5, 1, 7, 1, 7, 6, 2, 2, 7, 3, 4] and 5 tills ==> expected: <14> but was: <15>
+    @Test
+    void random1() {
+        assertEquals(14, SuperMarketQueue.solveSuperMarketQueue(new int[]{6, 1, 1, 2, 5, 1, 7, 1, 7, 6, 2, 2, 7, 3, 4}, 5));
+    }
+
+    // For customers [1, 5, 1, 5, 7, 2, 4, 3, 3, 2, 2, 5, 3, 2, 6, 7, 3] and 2 tills ==> expected: <31> but was: <32>
+    @Test
+    void random2() {
+        assertEquals(31, SuperMarketQueue.solveSuperMarketQueue(new int[]{1, 5, 1, 5, 7, 2, 4, 3, 3, 2, 2, 5, 3, 2, 6, 7, 3}, 2));
+    }
+
+    //For customers {29, 18, 6, 23, 25, 29, 24, 17, 10, 8, 8, 22, 20, 16, 13, 17, 7, 21, 7, 11, 18, 26, 25, 1, 18, 29, 16, 26, 7, 11, 13, 20, 12, 6, 23, 3, 10, 9, 8, 5, 6, 18, 19, 26, 5, 15, 4, 15, 1, 4} and 7 tills ==> expected: <113> but was: <119>
+    @Test
+    void random3() {
+        assertEquals(113, SuperMarketQueue.solveSuperMarketQueue(new int[]{29, 18, 6, 23, 25, 29, 24, 17, 10, 8, 8, 22, 20, 16, 13, 17, 7, 21, 7, 11, 18, 26, 25, 1, 18, 29, 16, 26, 7, 11, 13, 20, 12, 6, 23, 3, 10, 9, 8, 5, 6, 18, 19, 26, 5, 15, 4, 15, 1, 4}, 7));
+    }
+
 }
+
+/*
+    For customers [6, 1, 1, 2, 5, 1, 7, 1, 7, 6, 2, 2, 7, 3, 4] and 5 tills ==> expected: <14> but was: <15>
+
+t1      6, 1, 1, 2, 5, 1, 7, 1, 7, 6,               2, 2, 7, 3, 4
+t2                                                  1  1  6  2  3
+t3                                                  0  0  5   1 2
+
+
+[INFO] SuperMarketQueue - round 1  till(0): 3 till(1): 3 queue : 4
+[INFO] SuperMarketQueue - round 2  till(0): 2 till(1): 2 queue : 4
+[INFO] SuperMarketQueue - round 3  till(0): 1 till(1): 1 queue : 4
+[INFO] SuperMarketQueue - round 4  till(0): 0 till(1): 0 queue : 4
+[INFO] SuperMarketQueue - round 5  till(0): 2 till(1): 2 queue : 2
+[INFO] SuperMarketQueue - round 6  till(0): 1 till(1): 1 queue : 2
+[INFO] SuperMarketQueue - round 7  till(0): 0 till(1): 0 queue : 2
+[INFO] SuperMarketQueue - round 8  till(0): 1 till(1): 1 queue : 0
+[INFO] SuperMarketQueue - round 9  till(0): 0 till(1): 0 queue : 0
+
+
+[INFO] SuperMarketQueue - round 1  till(0): 3 till(1): 2 till(2): 6 till(3): 1 till(4): 1 queue : 10
+[INFO] SuperMarketQueue - round 2  till(0): 2 till(1): 1 till(2): 5 till(3): 0 till(4): 0 queue : 10
+[INFO] SuperMarketQueue - round 3  till(0): 1 till(1): 0 till(2): 4 till(3): 5 till(4): 6 queue : 8
+[INFO] SuperMarketQueue - round 4  till(0): 0 till(1): 0 till(2): 3 till(3): 4 till(4): 5 queue : 7
+[INFO] SuperMarketQueue - round 5  till(0): 6 till(1): 0 till(2): 2 till(3): 3 till(4): 4 queue : 5
+[INFO] SuperMarketQueue - round 6  till(0): 5 till(1): 4 till(2): 1 till(3): 2 till(4): 3 queue : 4
+[INFO] SuperMarketQueue - round 7  till(0): 4 till(1): 3 till(2): 0 till(3): 1 till(4): 2 queue : 4
+[INFO] SuperMarketQueue - round 8  till(0): 3 till(1): 2 till(2): 1 till(3): 0 till(4): 1 queue : 3
+[INFO] SuperMarketQueue - round 9  till(0): 2 till(1): 1 till(2): 0 till(3): 0 till(4): 0 queue : 2
+[INFO] SuperMarketQueue - round 10  till(0): 1 till(1): 0 till(2): 0 till(3): 5 till(4): 0 queue : 0
+[INFO] SuperMarketQueue - round 11  till(0): 0 till(1): 0 till(2): 0 till(3): 4 till(4): 0 queue : 0
+[INFO] SuperMarketQueue - round 12  till(0): 0 till(1): 0 till(2): 0 till(3): 3 till(4): 0 queue : 0
+[INFO] SuperMarketQueue - round 13  till(0): 0 till(1): 0 till(2): 0 till(3): 2 till(4): 0 queue : 0
+[INFO] SuperMarketQueue - round 14  till(0): 0 till(1): 0 till(2): 0 till(3): 1 till(4): 0 queue : 0
+[INFO] SuperMarketQueue - round 15  till(0): 0 till(1): 0 till(2): 0 till(3): 0 till(4): 0 queue : 0
+
+ */
