636b25fe56bf67daad8f3b043215165817a18b87
PONCHON Marc <marc.ponchon@orange.com>
2025-04-10 08:44:52 +0200

    Printer

---
+++ java/test/java/katas/Kyu7/PrinterTest.java
@@ -0,0 +1,15 @@
+package katas.Kyu7;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrinterTest {
+
+    @Test
+    public void test() {
+        System.out.println("printerError Fixed Tests");
+        String s="aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz";
+        assertEquals("3/56", Printer.printerError(s));
+    }
+}
